122761160e2246cce274d9fa2a41e8475e171e9b
PratamaAngga <anggashillnoir@gmail.com>
2024-10-04 22:03:13 +0700

    Experiment 1 question 3

--- Experiment/Selection2Exp120.java
+++ Experiment/Selection2Exp120.java
@@ -4,5 +4,19 @@ public class Selection2Exp120 {
         Scanner input20 = new Scanner(System.in);
 
         int year;
+
+        System.out.println();
+        System.out.print("Input year = ");
+        year = input20.nextInt();
+
+        if (year%4 == 0) {
+            if (year%100 != 0) {
+                System.out.println("Leap year");
+            } else {
+                System.out.println("Not a leap year");
+            }
+        } else {
+            System.out.println("Not a leap year");
+        }
     }
 }
