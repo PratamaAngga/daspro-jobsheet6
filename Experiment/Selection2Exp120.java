ed34561bca869b90fa338d40d9c231813927bf9c
PratamaAngga <anggashillnoir@gmail.com>
2024-10-04 21:34:06 +0700

    try file in folder

---
+++ Experiment/Selection2Exp120.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Selection2Exp120 {
+    public static void main(String[] args) {
+        Scanner input20 = new Scanner(System.in);
+
+        int year;
+    }
+}
