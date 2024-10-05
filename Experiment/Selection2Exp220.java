28ce899f4c7d710536b29d072a276b01386f7a65
PratamaAngga <anggashillnoir@gmail.com>
2024-10-05 13:05:59 +0700

    Experiment 2 after question

--- Experiment/Selection2Exp220.java
+++ Experiment/Selection2Exp220.java
@@ -17,8 +17,12 @@ public class Selection2Exp220 {
         if (totalAngle == 180) {
             if (angle1 == 90 || angle2 == 90 || angle3 == 90) {
                 System.out.println("Right triangle");
+            } else if (angle1 == angle2 && angle2 == angle3) {
+                System.out.println("Equilateral triangle");
+            } else if (angle1 == angle2 || angle2 == angle3 || angle3 == angle1) {
+                System.out.println("Isosceles triangle");
             } else {
-                System.out.println("Not a right triangle");
+                System.out.println("Not a valid triangle");
             }
         } else {
             System.out.println("Not a triangle");
